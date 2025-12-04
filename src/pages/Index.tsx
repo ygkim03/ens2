3c7f3043b23ac03e38628c931572733f3eff6fe4
ygkim03 <rladudrnr03@gmail.com>
2025-12-04 11:56:17 +0900

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -147,12 +147,17 @@ const Index = () => {
             </div>
             <CollapsibleContent className="mt-1 text-xs text-muted-foreground space-y-0.5">
               <p>
-                <span className="font-semibold text-primary">ENS</span> ({workerData.ensCount}명) : 
+                <span className="font-semibold text-primary">ENS</span> ({workerData.ensCount}명)
+                {workerData.ensStatus === "교대 전" && (
+                <span className="text-yellow-600 ml-1">(교대 전)</span>
+                )} : 
                 <span className="text-[11px] leading-tight ml-1">{workerData.ensWorkers.map(w => w.name).join(', ')}
                    </span>
               </p>
               <p>
-                <span className="font-semibold text-accent">웨스턴</span> ({workerData.westCount}명) : {workerData.westWorkers.map(w => w.name).join(', ')}
+                <span className="font-semibold text-accent">웨스턴</span> ({workerData.westCount}명){workerData.westStatus === "교대 전" && (
+                <span className="text-yellow-600 ml-1">(교대 전)</span>
+                )} : {workerData.westWorkers.map(w => w.name).join(', ')}
               </p>
             </CollapsibleContent>
           </Collapsible>
